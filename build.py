cc6ffd19e6cb59b10764fbe5cc618730fe21ff08
Wyatt Gill <wyattfgill7@gmail.com>
2022-10-12 22:05:36 -0500

    Update build.py

--- build.py
+++ build.py
@@ -4,13 +4,11 @@ import datetime
 import subprocess
 
 
-image_tag = 'latest'
-
 docker_build_cmd = [
     'docker', 'build',
     '--build-arg', f'BUILD_DATE={str(datetime.datetime.utcnow())}',
-    '--build-arg', f'IMAGE_VERSION={image_tag}',
-    '--tag', f'ghcr.io/wfg/wireguard:{image_tag}',
+    '--build-arg', f'IMAGE_VERSION=testing',
+    '--tag', f'ghcr.io/wfg/wireguard:latest',
     './build',
 ]
 subprocess.run(docker_build_cmd)
