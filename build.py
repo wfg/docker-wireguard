bb6c69afb4203b50b589576ba75c5a056ff6986f
Wyatt Gill <wyattfgill7@gmail.com>
2023-03-12 12:42:18 -0500

    Bump Alpine version

--- build.py
+++ build.py
@@ -7,8 +7,8 @@ import subprocess
 docker_build_cmd = [
     'docker', 'build',
     '--build-arg', f'BUILD_DATE={str(datetime.datetime.utcnow())}',
-    '--build-arg', f'IMAGE_VERSION=testing',
-    '--tag', f'ghcr.io/wfg/wireguard:latest',
+    '--build-arg', 'IMAGE_VERSION=testing',
+    '--tag', 'ghcr.io/wfg/wireguard:latest',
     './build',
 ]
 subprocess.run(docker_build_cmd)
