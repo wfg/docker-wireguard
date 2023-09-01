8cc41440e5bdf56dfaef1fbd8abcc0aaf7521546
Wyatt Gill <wyatt@wyattgill.net>
2023-09-01 13:47:56 -0500

    Convert to Debian base

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -18,6 +18,9 @@ interface=$(basename "${config_file%.*}")
 # Bring up the WireGuard interface
 wg-quick up "$interface"
 
+# Gracefully exit when signalled
+trap 'cleanup $interface' SIGINT SIGTERM
+
 # > ...when used with interfaces that have a peer that specifies 0.0.0.0/0 as part of the ‘AllowedIPs’,
 # > [this iptables command] works together with wg-quick’s fwmark usage in order to drop all packets
 # > that are either not coming out of the tunnel encrypted or not going through the tunnel itself
@@ -31,15 +34,26 @@ iptables --insert OUTPUT \
     ! --destination "$(ip -4 -oneline addr show dev eth0 | awk 'NR == 1 { print $4 }')" \
     --jump REJECT
 
+# nft insert rule ip filter OUTPUT \
+#     oifname != "$interface" \
+#     mark != "$(wg show "$interface" fwmark)" \
+#     fib daddr type != local \
+#     ip daddr != "$(ip -4 -oneline addr show dev eth0 | awk 'NR == 1 { print $4 }')" \
+#     counter reject
+
+
 # Create static routes for any ALLOWED_SUBNETS and punch holes in the firewall
 default_gateway=$(ip -4 route | awk '$1 == "default" { print $3 }')
 for subnet in ${ALLOWED_SUBNETS//,/ }; do
     ip route add "$subnet" via "$default_gateway"
-    iptables --insert OUTPUT --destination "$subnet" --jump ACCEPT
-done
+    iptables --insert OUTPUT \
+        --destination "$subnet" \
+        --jump ACCEPT
 
-# Gracefully exit when signalled
-trap 'cleanup $interface' TERM
+    # nft insert rule ip filter OUTPUT \
+    #     ip daddr "$subnet" \
+    #     counter accept
+done
 
 sleep infinity &
 wait
